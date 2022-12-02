52c204b07212932ac854ce3ca73a247055cc89f6
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2022-12-02 11:30:00 +0100

    test: day 2 script

---
+++ test/day_2_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+
+import 'package:advent_of_code/2022/day_2/main.dart' as day_2;
+
+void main() {
+  group('Day 2 results', () {
+    test('for part 1 should be correct', () async {
+      final results = await day_2.getResults();
+      expect(results.item1, equals(13268));
+    });
+    test('for part 2 should be correct', () async {
+      final results = await day_2.getResults();
+      expect(results.item2, equals(15508));
+    });
+  });
+}
